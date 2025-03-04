490c478649f019dee3558ca6d024ffdfba937356
endus <137748797+endus09@users.noreply.github.com>
2025-03-03 20:13:51 -0700

    Finished Fuzzy Logic Class

--- src/FuzzyLogic.java
+++ src/FuzzyLogic.java
@@ -1,8 +1,3 @@
-package net.sourceforge.jFuzzyLogic.test;
-
-import net.sourceforge.jFuzzyLogic.FIS;
-import net.sourceforge.jFuzzyLogic.rule.FuzzyRuleSet;
-
 /*
  @author Markus
 */
@@ -10,55 +5,26 @@ import net.sourceforge.jFuzzyLogic.rule.FuzzyRuleSet;
     public class FuzzyLogic{
 
         // declaring needed variables
-        float bobHunger;
-        float fredHunger;
-        Variable bobV;
-        Variable fredV;
-        FIS bob;
-        FIS fred;
+        double a,b;
 
-        // constructor (still needs work)
-        public FuzzyLogic() {
-            bob = FIS.load("fuzzy.fcl", true);
-            if (bob == null) {
-                System.err.println("error loading FCL file");
-                return;
-            }
-            fred = FIS.load("fuzzy.fcl", true);
-            if (fred == null) {
-                System.err.println("error loading FCL file");
-                return;
-            }
 
-            // setting values for people
-
-            bob.setVariable("activity", 5);
-            bob.setVariable("time",3.5);
-
-            fred.setVariable("activity", 3);
-            fred.setVariable("time",6);
-
-            bob.evaluate();
-            fred.evaluate();
-
-            bobV = bob.getVariable("hunger");
-            fredV = bob.getVariable("hunger");
-
-            bobHunger = bobV.getValue();
-            fredHunger = fredV.getValue();
+        // constructor
+        public FuzzyLogic( double mainValue, double secondaryValue) {
+            a = mainValue;
+            b = secondaryValue;
         }
 
         // methods for set operations
-        public float negation(float value){
-            return 1 - value;
+        public double negation(){
+            return 1 - a;
         }
 
-        public float union(float x,float y){
-            return x*y;
+        public double union(){
+            return a*b;
         }
 
-        public float intersection(float x,float y){
-            float isct = 1 - ((1-x) * (1-y));
+        public double intersection(){
+            double isct = 1 - ((1-a) * (1-b));
             return isct;
         }
     }
