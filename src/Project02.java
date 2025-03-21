d1c0c69a02300b7fef51a2c03ffcd6be2a003ce7
endus <137748797+endus09@users.noreply.github.com>
2025-03-21 17:45:03 -0600

    FINAL REVISION

--- src/Project02.java
+++ src/Project02.java
@@ -1,28 +1,29 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
-/** 
- * Test class for ICallBullSet and FuzzyLogic classes.
- * This class tests the ICallBullSet and FuzzyLogic classes by creating two sets of characters and integers
- * and performing set operations on them. It also tests the FuzzyLogic class by creating two FuzzyLogic objects
- * and performing set operations on them.
- * 
- * 
-* @author Edwin Casady
-* @author ADD YOUR NAME!!!!!!!!!
+/**
+ * Test class for ICallBullSet and FuzzyLogic classes. This class tests the
+ * ICallBullSet and FuzzyLogic classes by creating two sets of characters and
+ * integers and performing set operations on them. It also tests the FuzzyLogic
+ * class by creating two FuzzyLogic objects and performing set operations on
+ * them.
+ *
+ *
+ * @author Edwin Casady
  * @author revised by Jesus
-* @author revised by Markus
-* @author revised by Corbin
-*/
+ * @author revised by Markus
+ * @author revised by Corbin
+ */
+public class Project02
+{
 
-public class Project02 {
-
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
 
         /*----------------------------TESTING CLIENT-----------------------------*/
         ICallBullSet<Character, Integer> set1 = new ICallBullSet<>();
         ICallBullSet<Character, Integer> set2 = new ICallBullSet<>();
-
 
         FuzzyLogic fuz1 = new FuzzyLogic(.3);
         FuzzyLogic fuz2 = new FuzzyLogic(.8);
@@ -31,28 +32,32 @@ public class Project02 {
         // {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r}
         List<Character> setDomain = new ArrayList<>();
         char start = 'a';
-        for (int i = 0; i < 18; i++) {
+        for (int i = 0; i < 18; i++)
+        {
             setDomain.add(start++);
         }
-        
-         // This is to initialize the sets with the domain.
+
+        // This is to initialize the sets with the domain.
         // This does not assign any values to the sets, just keys
-        for (Character c : setDomain) {
+        for (Character c : setDomain)
+        {
             set1.put(c, 0);
             set2.put(c, 0);
         }
 
         // Sets values for set1 to every other character in the domain
         // {a, c, e, g, i, k, m, o, q}
-        for (int i = 'a'; i <= 'r'; i+=2) {
-            set1.put( (char)i, set1.get( (char)i ) +1);
-            
+        for (int i = 'a'; i <= 'r'; i += 2)
+        {
+            set1.put((char) i, set1.get((char) i) + 1);
+
         }
 
         // Sets values for set2 to every other character in the domain
         // {b, d, f, h, j, l, n, p, r}
-        for (int i = 'b'; i <= 'r'; i+=2) {
-            set2.put( (char)i, set2.get( (char)i ) +1);
+        for (int i = 'b'; i <= 'r'; i += 2)
+        {
+            set2.put((char) i, set2.get((char) i) + 1);
         }
 
         System.out.println(); // Padding for console output
@@ -64,13 +69,13 @@ public class Project02 {
         System.out.println("Set1 init:  " + set1);
         System.out.println("Set1.not(): " + set1.not());
         System.out.println();
-        
+
         System.out.println("Set2 init:  " + set2);
         System.out.println("set2.not(): " + set2.not());
         System.out.println();
 
-        ICallBullSet<Character, Integer> set3 =  set1.union(set2);
-        ICallBullSet<Character, Integer> set4 =  set1.intersects(set2);
+        ICallBullSet<Character, Integer> set3 = set1.union(set2);
+        ICallBullSet<Character, Integer> set4 = set1.intersects(set2);
 
         System.out.println("Set1:             " + set1);
         System.out.println("Set2:             " + set2);
@@ -78,8 +83,8 @@ public class Project02 {
         System.out.println("set1.union(set2): " + set3);
         System.out.println("set1.inter(set2): " + set4);
 
-        System.out.println("set1.diffr(set2): " +  set1.difference(set2));
-        System.out.println("set1.symdf(set2): " +  set1.symmetricDifference(set2));
+        System.out.println("set1.diffr(set2): " + set1.difference(set2));
+        System.out.println("set1.symdf(set2): " + set1.symmetricDifference(set2));
         // The Project02 document doesn't req. a sum function for regular sets
 
         System.out.println(); // Padding for console output
@@ -93,18 +98,16 @@ public class Project02 {
         set1.put('c', 5);
         set1.put('r', 2);
 
-
         System.out.println("Set1 init:  " + set1);
         System.out.println("Set1.not(): " + set1.not());
         System.out.println();
-        
-        
+
         System.out.println("Set2 init:  " + set2);
         System.out.println("set2.not(): " + set2.not());
         System.out.println();
 
-         set3 =  set1.union(set2);
-         set4 =  set1.intersects(set2);
+        set3 = set1.union(set2);
+        set4 = set1.intersects(set2);
 
         System.out.println("Set1:             " + set1);
         System.out.println("Set2:             " + set2);
@@ -112,52 +115,39 @@ public class Project02 {
         System.out.println("set1.union(set2): " + set3);
         System.out.println("set1.inter(set2): " + set4);
 
-        System.out.println("set1.diffr(set2): " +  set1.difference(set2));
-        System.out.println("set1.sum(set2):   " +  set1.sum(set2));
+        System.out.println("set1.diffr(set2): " + set1.difference(set2));
+        System.out.println("set1.sum(set2):   " + set1.sum(set2));
+        System.out.println("set1.symdif(set2):" + set1.symmetricDifference(set2));
 
         System.out.println();
 
-            //----Symetric Difference----
+        // Fuzzy Logic
+        System.out.println("FUZZY LOGIC TESTING: ");
 
-            System.out.println("Symmetric Difference test:");
-            System.out.println("Set1 & Set2 Symmetric Difference: " + set1.symmetricDifference(set2));
-
-            System.out.println();
-        System.out.println("Fuzzy Logic Testing: ");
-       
         System.out.println("A = " + fuz1.get());
         System.out.println("B = " + fuz2.get());
-        System.out.printf("Negation for A: %2.2f\n" , fuz1.negation() );
-        System.out.printf("Negation for B: %2.2f\n" , fuz2.negation());
-        System.out.printf("A union B: %2.2f\n" , fuz1.union(fuz2));
-        System.out.printf("A intersection B: %2.2f\n" , fuz1.intersection(fuz2));
+        System.out.println();
+        System.out.printf("Negation for A: %2.2f\n", fuz1.negation());
+        System.out.printf("Negation for B: %2.2f\n", fuz2.negation());
+        System.out.println();
+        System.out.printf("A union B: %2.2f\n", fuz1.union(fuz2));
+        System.out.printf("A intersection B: %2.2f\n", fuz1.intersection(fuz2));
 
         //-----More Fuzzy Logic Testing-------
-            System.out.println(" more Fuzzy Logic Testing:");
-            System.out.println();
-            FuzzyLogic fuzzyZero = new FuzzyLogic(0.0);
-            FuzzyLogic fuzzyOne = new FuzzyLogic(1.0);
+        System.out.println();
+        FuzzyLogic fuzzyZero = new FuzzyLogic(0.0);
+        FuzzyLogic fuzzyOne = new FuzzyLogic(1.0);
 
-            System.out.println("fuzzyZero: " + fuzzyZero.get());
-            System.out.println("fuzzyOne: " + fuzzyOne.get());
-            System.out.println();
-            //testing to show values less than or higher than one are handled
-            FuzzyLogic fuzzyLessThanZero = new FuzzyLogic(-0.5);
-            FuzzyLogic FuzzyAboveOne = new FuzzyLogic(1.5);
-            System.out.println("FuzzyLessThanZero: " + fuzzyLessThanZero.get() + "(initially -0.5)");
-            System.out.println("FuzzyAboveOne: " + FuzzyAboveOne.get() + "(initially 1.5)");
-            System.out.println();
+        System.out.println("fuzzyZero: " + fuzzyZero.get());
+        System.out.println("fuzzyOne: " + fuzzyOne.get());
+        System.out.println();
 
-            FuzzyLogic settingFuzzy = new FuzzyLogic(0.3);
-            System.out.println("settingFuzzy before set method: " + settingFuzzy.get());
-            settingFuzzy.set(0.9);
-            System.out.println("settingFuzzy after set method: " + settingFuzzy.get());
-            settingFuzzy.set(-0.4); //testing for the case in which the value is set to a negative
-            System.out.println("settingFuzzy if set negative: " + settingFuzzy.get());
-            settingFuzzy.set(2.0);
-            System.out.println("SettingFuzzy if set higher than 1: " + settingFuzzy.get());
-            System.out.println();
-
+        //testing to show values less than or higher than one are handled
+        FuzzyLogic fuzzyLessThanZero = new FuzzyLogic(-0.5);
+        FuzzyLogic FuzzyAboveOne = new FuzzyLogic(1.5);
+        System.out.println("fuzzyLessThanZero: " + fuzzyLessThanZero.get() + "(initially -0.5)");
+        System.out.println("fuzzyAboveOne: " + FuzzyAboveOne.get() + "(initially 1.5)");
+        System.out.println();
 
     }
 }
